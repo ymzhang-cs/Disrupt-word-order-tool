f65fc30b46b46a402f93c0df35bad86db3b0d345
ZhangYuanming <ZhangY.M.0533@gmail.com>
2019-06-02 10:49:34 +0800

    Realease version 0.2.5

--- graph.py
+++ graph.py
@@ -1,14 +1,15 @@
 from order import order
 import tkinter as tk
+import os
 
 window = tk.Tk()
 window.title("英语句子打乱工具")
-window.geometry("300x170")
+window.geometry("300x180")
 
 inputNotice = tk.Label(window, text="↓ Please Input ↓", font=('Times New Roman', 12))
 
 e = tk.Entry(window, show=None)
-
+e.focus_set()
 text = tk.StringVar()
 
 def ordering():
@@ -18,13 +19,19 @@ def ordering():
     text.set(orderedText)
     t.insert('insert', text.get())
 
+def copyText():
+    os.system("echo '%s' | clip" % text.get())
+
 b = tk.Button(window, text="START", font=('Times New Roman', 12), width=12, height=1, command=ordering)
 
 t = tk.Text(window, height = 5)
+
+copyButton = tk.Button(window, text="复制", font=('微软雅黑', 10), width=8, height=1,command=copyText)
 
 inputNotice.pack()  # 显示"↓ Please Input ↓"
 e.pack()            # 显示输入框
 b.pack()            # 显示按钮
 t.pack()            # 显示文本框
+copyButton.pack()   # 显示复制按钮
 
 window.mainloop()
